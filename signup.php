969b9442e950eea04f710be6f007f66f356428ec
oluwamayowaf <e_fadairo@yahoo.com>
2019-09-16 17:23:14 +0100

    Added signup.php and style for signup page

---
+++ signup.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Heroes Login Page</title>
+    <link rel="stylesheet" type="text/css" href="css/signup.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Londrina+Shadow|Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/942fa82be2.js"></script>
+
+</head>
+
+<body>
+    <div class="bg">
+        <div class="first-box">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="signup">
+                        <div class="logo">
+                            <img src="https://res.cloudinary.com/ddtech/image/upload/v1568622070/Team%20Heroes/team_heroes_logo_y2hlv4.png" class="center" alt="team_logo">
+
+                        </div>
+
+                        <form>
+
+                            <div class="form-group row">
+
+                                <div class="col-auto">
+                                    <input type="text" class="form-control" placeholder="Full Name " id="fullname" name="fullname">
+
+                                </div>
+
+                            </div>
+                            <hr>
+
+                            <div class="form-group row">
+
+                                <div class="col-xs-9">
+                                    <input type="text" class="form-control" id="email" placeholder="Email" name="email">
+                                </div>
+
+                            </div>
+                            <hr>
+                            <div class="form-group row">
+
+                                <div class="col-auto">
+                                    <input type="text" class="form-control " placeholder="Password" id="password" name="password">
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="form-group row">
+
+                                <div class="col-xs-9">
+                                    <input type="text" class="form-control " placeholder="Confirm Password" id="confirmpassword" name="confirmpassword">
+
+                                </div>
+
+                            </div>
+                            <hr>
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="terms">
+                                <label class="form-check-label" for="terms">I agree to the <a href="#">Terms and Conditions</a></label>
+                            </div>
+                            <div class="form-group row float-right">
+                                <div class="offset-xs-3 col-xs-9 ">
+                                    <button type="submit" class="btn btn-primary" style="background-color: #E8716D; border-color:#E8716D; ">Sign
+                                        Up</button>
+                                </div>
+                            </div>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+            <!-- Login Text Block-->
+
+        </div>
+
+
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+</html>
